cd94263eeda11146978ef64306e2b466c506f70f
oylsister <57207701+oylsister@users.noreply.github.com>
2026-07-26 13:33:17 +0700

    Remove player bottom model for more visibility

--- ZombieModSharp/Core/Modules/PlayerClasses.cs
+++ ZombieModSharp/Core/Modules/PlayerClasses.cs
@@ -170,7 +170,7 @@ public class PlayerClasses : IPlayerClasses
 
         // some map item set alpha to 0, which will make the player invisible. So we need to set the alpha to 1.
         //playerPawn.AcceptInput("Alpha", playerPawn, playerPawn, 255);
-        playerPawn.RenderColor = new Color32(255, 255, 255, 255);
+        playerPawn.RenderColor = new Color32(255, 255, 255, 254);
 
         if (classAttribute.Model != "default" && !string.IsNullOrEmpty(classAttribute.Model))
             playerPawn.SetModel(classAttribute.Model);
